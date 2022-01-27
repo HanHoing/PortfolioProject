c82d61cb269d1a902d62ebece68f8994b820b154
sshh0719 <hansh011@naver.com>
2022-01-27 18:30:45 +0900

    상희 첨으로 커밋해

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -3,6 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
+  //상희꺼후에엥
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
