f17904e83ebcf24f1c5404fe7ced10db7461e83f
sshh0719 <hansh011@naver.com>
2022-01-27 18:31:37 +0900

    후엥

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -3,7 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  //상희꺼후에엥
+  //상희꺼후에엥히잉
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
