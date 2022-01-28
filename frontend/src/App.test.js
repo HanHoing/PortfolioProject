f559c18cd30f52f3c2807b047fb204fd2432d24e
sshh0719 <hansh011@naver.com>
2022-01-28 10:15:44 +0900

    two commit

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -3,7 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  //상희꺼후에엥히잉
+  //상희꺼후에엥히잉 호호호?
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
