cc3e4469127806da4eade5d923c9eef358b4dc22
HanHoing <hhy6789@gmail.com>
2022-02-11 09:55:05 +0900

    whs경로추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Main from 'Main/Main';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HHY from 'HHY/Hhy';
+import WHS from 'WHS/Whs';  //css 중첩으로 main 흰색됨ㅎ..
 
 
 class App extends Component {
@@ -13,10 +14,10 @@ class App extends Component {
         <BrowserRouter>
         <Routes>
           <Route path="/" element={<Main  />}></Route>
-          {/* <Route path="/sjw" element={<SJW />}></Route>
-          <Route path="/ihy" element={<IHY />}></Route>
+          {/* <Route path="/sjw" element={<SJW />}></Route> */}
+          {/* <Route path="/ihy" element={<IHY />}></Route> */}
           <Route path="/whs" element={<WHS />}></Route>
-          <Route path="/hsh" element={<HSH />}></Route> */}
+          {/* <Route path="/hsh" element={<HSH />}></Route>  */}
           <Route path="/hhy" element={<HHY />}></Route>
         </Routes>
       </BrowserRouter>
